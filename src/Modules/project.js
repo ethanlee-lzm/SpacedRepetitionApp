f3d9d9b652c5ab21a1dfa6b6bf4ebabb101c2339
Ethan Lee <79266746+ethanlee-lzm@users.noreply.github.com>
2021-06-05 09:28:11 -0400

    Basic implementation of Project and Task classes

---
+++ src/Modules/project.js
@@ -0,0 +1,26 @@
+// factory
+
+class Task {
+    constructor(name, dueDate = "No date") {
+      this.name = name;
+      this.dueDate = dueDate;
+    }
+  
+    setName(name) {
+      this.name = name;
+    }
+  
+    getName() {
+      return this.name;
+    }
+  
+    setDate(dueDate) {
+      this.dueDate = dueDate;
+    }
+  
+    getDate() {
+      return this.dueDate;
+    }
+  }
+  
+  export default Task;
---
+++ src/Modules/userInterface.js
@@ -0,0 +1,17 @@
+//module
+
+class UserInterface {
+    //load projects from local storage or load default projects if empty
+  
+    //add project
+    //delete project
+    //rename project
+    //open project
+  
+    //add task
+    //delete task
+    //rename task
+    //change task date
+  
+    constructor() {}
+  }
